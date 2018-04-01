61e4ea9652a769c0d2c8fca19f1b49ae25f71a8a
Paul <Paul@wolf.Home>
2018-04-01 15:52:16 +0200

    Comit 10

--- HolaGit/src/HolaGitClase.java
+++ HolaGit/src/HolaGitClase.java
@@ -7,7 +7,6 @@ public class HolaGitClase {
 		System.out.println("Hola Mundo 3");
 		System.out.println("Hola Mundo 4");
 		System.out.println("Va Bien :v");
-		System.out.println("Hola Mundo 5");
 	}
 
 }
