6397480576c7e98f31c7c71137515cc24b100756
Paul <Paul@wolf.Home>
2018-04-01 15:40:57 +0200

    Comit 8

--- HolaGit/src/HolaGitClase.java
+++ HolaGit/src/HolaGitClase.java
@@ -4,7 +4,6 @@ public class HolaGitClase {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Mundo 2");
-		System.out.println("Hola Mundo 3");
 		System.out.println("Hola Mundo 4");
 		System.out.println("Va Bien :v");
 		System.out.println("Hola Mundo 5");
